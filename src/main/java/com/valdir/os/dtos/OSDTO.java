1a0337c303a87f3336ca056471ad139d4b983ed9
GutoBruxel <106287817+GutoBruxel@users.noreply.github.com>
2022-06-06 16:26:50 -0300

    Update OSDTO.java

--- src/main/java/com/valdir/os/dtos/OSDTO.java
+++ src/main/java/com/valdir/os/dtos/OSDTO.java
@@ -84,8 +84,8 @@ public class OSDTO implements Serializable {
 		this.observacoes = observacoes;
 	}
 
-	public Integer getStatus() {
-		return status;
+	public Status getStatus() {
+		return Status.toEnum(this.status);
 	}
 
 	public void setStatus(Integer status) {
